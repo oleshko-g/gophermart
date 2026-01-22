c4fb90c840a8145ab3955f7a61a40028b1d450e7
Gennady Oleshko <gennady@oleshko-g.ru>
2026-01-22 21:04:16 +0300

    add test cases to balance_test
    Signed-off-by: Gennady Oleshko <gennady@oleshko-g.ru>

--- internal/service/balance/balance_test.go
+++ internal/service/balance/balance_test.go
@@ -6,12 +6,12 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
+	moqAccrual "github.com/oleshko-g/oggophermart/internal/gen/transport/moq/accrual"
 	"github.com/oleshko-g/oggophermart/internal/oglog"
 	_ "github.com/oleshko-g/oggophermart/internal/storage"
 	"github.com/oleshko-g/oggophermart/internal/storage/db"
 	"github.com/oleshko-g/oggophermart/internal/storage/db/sql"
-	moqAccrual "github.com/oleshko-g/oggophermart/internal/gen/transport/moq/accrual"
-
+	"github.com/oleshko-g/oggophermart/internal/transport"
 )
 
 var (
@@ -29,6 +29,16 @@ func TestMain(m *testing.M) {
 }
 
 func Test_processAccrual(t *testing.T) {
+	cases := struct {
+		name        string
+		orderNumber string
+		FetchOrderAccrualFunc fetchOrderAccrualFunc
+	}{
+		name: "empty order accrual",
+		orderNumber: "123",
+		FetchOrderAccrualFunc: emptyFetchOrderAccrualFunc,
+	}
+	_ = cases
 	orderID, _ := uuid.NewRandom()
 	ctx, cancel := context.WithCancelCause(context.Background())
 	defer cancel(nil)
@@ -45,4 +55,10 @@ func configueStorage() *db.Config {
 	cfg.DSN().Set("postgres://gennadyoleshko:@localhost:5432/oggophermart?sslmode=disable")
 
 	return &cfg
+}
+
+type fetchOrderAccrualFunc func(context.Context, transport.FetchOrderAccrualPayload) (*transport.FetchOrderAccrualResult, error)
+
+func emptyFetchOrderAccrualFunc(ctx context.Context, payload transport.FetchOrderAccrualPayload) (*transport.FetchOrderAccrualResult, error) {
+	return nil, nil
 }
