898f805a84e4bea8b4ddea308f44a1059f47bfa1
Gennady Oleshko <gennady@oleshko-g.ru>
2026-01-29 10:08:42 +0300

    add WithLogEndpoint middleware
    Signed-off-by: Gennady Oleshko <gennady@oleshko-g.ru>

--- internal/service/balance/balance.go
+++ internal/service/balance/balance.go
@@ -16,6 +16,7 @@ import (
 	storageErrors "github.com/oleshko-g/oggophermart/internal/storage/errors"
 	"github.com/oleshko-g/oggophermart/internal/transport"
 	"goa.design/clue/log"
+	goa "goa.design/goa/v3/pkg"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -329,4 +330,20 @@ func accrualStatusToOrderStatus(accrualStatus transport.OrderAccrualStatus) (str
 		return OrderStatusProcessed, nil
 	}
 	return "", ErrUnknownAccrualOrderStatus
+}
+
+func WithLogEndpoint(endpoint goa.Endpoint) goa.Endpoint {
+	return func(ctx context.Context, payload any) (result any, err error) {
+		log.MustContainLogger(ctx)
+
+		p := fmt.Sprintf("%+v", payload)
+		log.Debug(ctx, log.KV{K: "payload", V: p})
+
+		result, err = endpoint(ctx, payload)
+
+		r := fmt.Sprintf("%+v", result)
+		log.Debug(ctx, log.KV{K: "result", V: r}, log.KV{K: "error", V: err})
+
+		return result, err
+	}
 }
--- internal/transport/http/server.go
+++ internal/transport/http/server.go
@@ -8,7 +8,8 @@ import (
 	"net/http"
 	"time"
 
-	balance "github.com/oleshko-g/oggophermart/internal/gen/balance"
+	genBalance "github.com/oleshko-g/oggophermart/internal/gen/balance"
+	balance "github.com/oleshko-g/oggophermart/internal/service/balance"
 	genBalanceHTTPSrv "github.com/oleshko-g/oggophermart/internal/gen/http/balance/server"
 	genUserHTTPSvr "github.com/oleshko-g/oggophermart/internal/gen/http/user/server"
 	user "github.com/oleshko-g/oggophermart/internal/gen/user"
@@ -29,7 +30,7 @@ type server struct {
 	Server
 }
 
-func newHandlers(loggingCtx context.Context, balanceEndpoints *balance.Endpoints, userEndpoints *user.Endpoints) http.Handler {
+func newHandlers(loggingCtx context.Context, balanceEndpoints *genBalance.Endpoints, userEndpoints *user.Endpoints) http.Handler {
 	var (
 		reqDecoder func(r *http.Request) goahttp.Decoder
 		resEncoder func(ctx context.Context, res http.ResponseWriter) goahttp.Encoder
@@ -60,13 +61,16 @@ func newHandlers(loggingCtx context.Context, balanceEndpoints *balance.Endpoints
 
 func NewServer(loggingCtx context.Context, cfg Config, svc service.Service) Server {
 	var (
-		balanceEndpoints *balance.Endpoints
+		balanceEndpoints *genBalance.Endpoints
 		userEndpoints    *user.Endpoints
 		handlers         http.Handler
 	)
 	{
-		balanceEndpoints = balance.NewEndpoints(svc.Balance)
+		balanceEndpoints = genBalance.NewEndpoints(svc.Balance)
+		balanceEndpoints.Use(balance.WithLogEndpoint)
+
 		userEndpoints = user.NewEndpoints(svc.User)
+
 		handlers = newHandlers(loggingCtx, balanceEndpoints, userEndpoints)
 	}
 
