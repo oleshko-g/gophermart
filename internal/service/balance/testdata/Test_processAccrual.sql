fb773edd2ea52d547e0d9cb77b21f629ba7f1940
Gennady Oleshko <gennady@oleshko-g.ru>
2026-02-07 15:32:56 +0300

    add orders for each case of Test_processAccrual
    Signed-off-by: Gennady Oleshko <gennady@oleshko-g.ru>

--- internal/service/balance/testdata/Test_processAccrual.sql
+++ internal/service/balance/testdata/Test_processAccrual.sql
@@ -10,78 +10,46 @@ INSERT INTO
 VALUES
   (
     '019c22a8e55179a4af55ace1ea6cdf68',
-    'pWNAiTM8LVTdB',
+    'user_Test_pocessAccrual',
     '$2a$10$uIdSMKOJ/dLQfsPDhE7/mOvGcqu0CgSAJ.US9VsmOeq.IJl5RoRMy',
     '2026-02-03 11:40:20.049632+03',
     '2026-02-03 11:40:20.049632+03',
     NULL
-  ),
-  (
-    '019c22a8e9ab76fd9f93b5d5b536709d',
-    'Hrdi3',
-    '$2a$10$ffSwQQCvgyiCYp1mbCcKdOnBlzpJSw4Ek4n1vSEhTa1oDRfjlxZtm',
-    '2026-02-03 11:40:21.163458+03',
-    '2026-02-03 11:40:21.163458+03',
-    NULL
-  ),
-  (
-    '019c22a8ea257d36968db83f4cc750ae',
-    'gs4wBnQA0H',
-    '$2a$10$lKwKcm7JQ3XkTf.Np/UxMuTbEo0fwqsDXXJFDx9j1KeFm02ytbEU6',
-    '2026-02-03 11:40:21.285866+03',
-    '2026-02-03 11:40:21.285866+03',
-    NULL
-  ),
-  (
-    '019c22a8ea687ad9be7c83069f5a0870',
-    'oseklbtFgg0T',
-    '$2a$10$teDimQlKmvvuzqa5Z30Zd.taXZm1gzkbGhK9zHSRUS9UHF/dmECFG',
-    '2026-02-03 11:40:21.352711+03',
-    '2026-02-03 11:40:21.352711+03',
-    NULL
   );
 
+-- user_Test_pocessAccrual
 INSERT INTO
   orders (id, number, user_id, STATUS, created_at)
 VALUES
+  -- no accrual
   (
     '019c22a8e94f7d75bcb973b05f070535',
     '568082882086285',
     '019c22a8e55179a4af55ace1ea6cdf68',
-    'PROCESSED',
+    'NEW',
     '2026-02-03 11:40:21.071876+03'
   ),
+  -- invalid accrual
   (
     '019c22a8ea2974ab87a39959d1301bf0',
     '73568464702',
-    '019c22a8ea257d36968db83f4cc750ae',
+    '019c22a8e55179a4af55ace1ea6cdf68',
     'NEW',
     '2026-02-03 11:40:21.289304+03'
   ),
+  -- PROCESSED no accrual
   (
     '019c22a8e55774419eae11fbe458db69',
     '167862623743756',
     '019c22a8e55179a4af55ace1ea6cdf68',
-    'PROCESSED',
+    'NEW',
     '2026-02-03 11:40:20.055278+03'
-  );
-
-INSERT INTO
-  transactions (id, user_id, order_id, kind, amount, created_at)
-VALUES
-  (
-    '019c22a8e8987cb2b8e5b4732c25bbf1',
-    '019c22a8e55179a4af55ace1ea6cdf68',
-    '019c22a8e55774419eae11fbe458db69',
-    'ACCRUAL',
-    72998,
-    '2026-02-03 11:40:20.888833+03'
   ),
+  -- PROCESSED with accrual
   (
-    '019c22a8e95076399f3d1ea92858ff11',
+    '019c3807-87e8-7a77-b9f8-d392ab5797df',
+    '2402188500348',
     '019c22a8e55179a4af55ace1ea6cdf68',
-    '019c22a8e94f7d75bcb973b05f070535',
-    'WITHDRAWAL',
-    10989,
-    '2026-02-03 11:40:21.072408+03'
+    'NEW',
+    '2026-02-07 15:15:43.592683+03'
   );
